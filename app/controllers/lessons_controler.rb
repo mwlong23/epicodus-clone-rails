20007a351cc4e57b4e20b18bea9225fc52f00a31
Mitchell <meechllada@gmail.com>
2017-11-14 17:37:50 -0800

    fix whitespace in controllers/lessons_controler

--- app/controllers/lessons_controler.rb
+++ app/controllers/lessons_controler.rb
@@ -1,6 +1,13 @@
 class LessonsController < ApplicationController
-  def new
+  def index
+    @course = Course.all
+    render :index
+  end
+
+  def show
     @course = Course.find(params[:course_id])
     @lesson = @course.lessons
+    render :show
   end
+
 end

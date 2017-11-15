6b6e54c3032844a8044a36cb6f9043caf33089e9
Mitchell <meechllada@gmail.com>
2017-11-14 17:22:17 -0800

    modify routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :courses, :lessons, :sections
+  resources :courses do
+    resources :lessons
+  end
+
+  resources :lessons do
+    resources :sections
+  end
+
 end
